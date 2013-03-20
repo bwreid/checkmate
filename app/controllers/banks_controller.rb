604250657ec1e3261a2d4a847ea1de197cf174de
Ben <benjamin_clayman@alumni.brown.edu>
2013-03-19 21:32:13 -0400

    boomcity

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,9 +1,13 @@
 class BanksController < ApplicationController
   def show
+    @bank = Bank.find(params[:id])
+  end
+
+  def index
   end
 
   def deposit_info
-    bank = Bank.find(params[:id])
+    bank = Bank.find(params[:bank_id])
     @deposits = bank.transactions.select{|x| x.is_deposit == true}
     @withdrawals = bank.transactions.select{|x| x.is_deposit == false}
   end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,10 @@
 class TransactionsController < ApplicationController
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    bank = Bank.find( params[:bank_id])
+    bank.transactions << Transaction.create( params[:transaction] )
+  end
 end
