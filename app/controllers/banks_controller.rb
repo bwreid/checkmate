83f16dcf8679dbfd629c92cec96d6f21ea62b38c
Shefali Friesen <musictechgirl@gmail.com>
2013-03-19 22:09:43 -0400

    transfer form

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -11,12 +11,19 @@ class BanksController < ApplicationController
     @banks = Bank.order(:name)
   end
 
-  def show
-    bank = Bank.find(params[:id])
+  def transfer
+    @banks = Bank.all
   end
 
-  def transfer
-    bank = Bank.find(params[:bank])
+  def transfer_amt
+    from = Bank.find(params[:from])
+    to = Bank.find(params[:to])
+    t1 = Transaction.create(amount:params[:amount], is_deposit:true, date:Date.current, notes:"deposit from #{from.name}")
+    to.transactions << t1
+    amt = params[:amount]
+    t2 = Transaction.create(amount:(amt - (amt*2)), is_deposit:false, date:Date.current, notes:"transfer to #{to.name}")
+    from.transactions << t2
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Checkmate::Application.routes.draw do
   root :to => 'home#index'
   resources :banks, :only => [:new, :create, :show, :destroy] do
     collection do
-      put :transfer
+      get :transfer
+      post :transfer_amt
     end
   end
 end
